988029c6ae35692dcd99968c18fa360b48433648
Allison Zadrozny <allison@zadrozny.com>
2023-02-09 22:52:58 -0600

    blargh.

--- mergesort.rb
+++ mergesort.rb
@@ -1,4 +1,4 @@
-ARRAY_LENGTH = 2
+ARRAY_LENGTH = 4
 
 # ARRAY = (0..50).to_a.sort{ rand() - 0.5 }[0..(ARRAY_LENGTH - 1)]
 
@@ -15,21 +15,29 @@ end
 
 # sort
 def merge(splits)
+
+  # need to loop until each piece is broken into 2-length arrays
+
   i = 0
   j = 0
   sorted_array = []
 
-  for k in 0..(ARRAY_LENGTH - 1) do
+  arr_a = splits[0]
+  arr_b = splits[1]
 
-    arr_a = splits[0]
-    arr_b = splits[1]
+  for k in 0..(ARRAY_LENGTH - 1) do
+    # do less indexing, and more splitting and concatenating
 
     puts "arr_a[i] < arr_b[j] => #{arr_a[i]} < #{arr_b[j]}"
 
-    if arr_a[i].nil? # end of arr_a
-      sorted_array[k] = arr_b[j]
+    if arr_a[i].nil?
+      # empty - sort rest of this array and concat
+      sorted_array.concat(sort(arr_b))
+      break
     elsif arr_b[j].nil?
-      sorted_array[k] = arr_a[i]
+      # empty - sort rest of this array and concat
+      sorted_array.concat(sort(arr_a))
+      break
     else
       if arr_a[i] < arr_b[j]
         sorted_array[k] = arr_a[i]
@@ -47,9 +55,17 @@ def merge(splits)
   sorted_array
 end
 
-# mergesort
-def mergesort(array)
-  splits = split(array)
+def sort(arr)
+  if arr.length > 1
+    splits = split(arr)
+    merge(splits)
+  else
+    arr
+  end
+end
 
+# mergesort - only works on arrays of length 2
+def mergesort(array)
+  splits = split(array) # how to recursively split and prepare for merge?
   merge(splits)
 end
