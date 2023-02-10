78a743b24b6c30b9e8b45b23c04079a234ac0f25
Allison Zadrozny <allison@zadrozny.com>
2023-02-09 22:56:52 -0600

    simpler methodology.

--- mergesort.rb
+++ mergesort.rb
@@ -1,71 +1,27 @@
-ARRAY_LENGTH = 4
+ARRAY_LENGTH = 8
 
-# ARRAY = (0..50).to_a.sort{ rand() - 0.5 }[0..(ARRAY_LENGTH - 1)]
+ARRAY = (0..50).to_a.sort{ rand() - 0.5 }[0..(ARRAY_LENGTH - 1)]
 
-# split
+def merge_sort(array)
+  return array if array.length <= 1
 
-def split(array)
-  # return array unless array.length > 1
-  # the split
-  [
-    array[0...(array.length / 2)],
-    array[(array.length / 2)..-1]
-  ]
+  middle = array.length / 2
+  left = array[0...middle]
+  right = array[middle..-1]
+
+  merge(merge_sort(left), merge_sort(right))
 end
 
-# sort
-def merge(splits)
+def merge(left, right)
+  sorted = []
 
-  # need to loop until each piece is broken into 2-length arrays
-
-  i = 0
-  j = 0
-  sorted_array = []
-
-  arr_a = splits[0]
-  arr_b = splits[1]
-
-  for k in 0..(ARRAY_LENGTH - 1) do
-    # do less indexing, and more splitting and concatenating
-
-    puts "arr_a[i] < arr_b[j] => #{arr_a[i]} < #{arr_b[j]}"
-
-    if arr_a[i].nil?
-      # empty - sort rest of this array and concat
-      sorted_array.concat(sort(arr_b))
-      break
-    elsif arr_b[j].nil?
-      # empty - sort rest of this array and concat
-      sorted_array.concat(sort(arr_a))
-      break
+  until left.empty? || right.empty?
+    if left.first <= right.first
+      sorted << left.shift
     else
-      if arr_a[i] < arr_b[j]
-        sorted_array[k] = arr_a[i]
-        puts "sorted_array: #{sorted_array}"
-        i += 1
-      else
-        sorted_array[k] = arr_b[j]
-        puts "sorted_array: #{sorted_array}"
-        j += 1
-      end
+      sorted << right.shift
     end
-
   end
 
-  sorted_array
-end
-
-def sort(arr)
-  if arr.length > 1
-    splits = split(arr)
-    merge(splits)
-  else
-    arr
-  end
-end
-
-# mergesort - only works on arrays of length 2
-def mergesort(array)
-  splits = split(array) # how to recursively split and prepare for merge?
-  merge(splits)
+  sorted.concat(left).concat(right)
 end
